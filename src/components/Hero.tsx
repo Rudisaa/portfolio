a444d6fcad658fb2c9cb2449bfa2c5356a08c7ea
Rudis <rudisarg@gmail.com>
2022-11-08 23:43:21 -0500

    added typing animation for header

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import Typewriter from 'typewriter-effect';
 
 const Hero = () => {
     return (
         <section id="hero">
-            Hero
+            <div className="welcome-msg">Welcome to my portfolio!</div>
+            <h1>
+                Hi! I'm Rudy a
+                <Typewriter onInit={(typewriter) => {
+                    typewriter.typeString('Computer Science Student')
+                        .pauseFor(1500)
+                        .deleteAll()
+                        .typeString('Cat Dad')
+                        .pauseFor(1000)
+                        .deleteAll()
+                        .typeString('Web Dev')
+                        .pauseFor(1000)
+                        .deleteAll()
+                        .typeString('skateborader')
+                        .deleteChars(5)
+                        .typeString('arder')
+                        .pauseFor(1000)
+                        .start();
+                }} options={{ loop: true, autoStart: true }} />
+            </h1>
         </section>
     )
 }
