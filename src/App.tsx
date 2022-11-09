907634f849c75fb0e1a946514c1c9057e4510674
Rudis <rudisarg@gmail.com>
2022-11-08 23:43:09 -0500

    added hero section to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import NavBar from './components/NavBar'
+import Hero from './components/Hero'
 import './App.css'
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
   return (
     <div className="App">
       <NavBar />
+      <Hero />
     </div>
   )
 }
